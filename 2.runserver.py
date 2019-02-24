ab6ef1531b15a06f0633262b6418685219797756
qoften <qoften@qq.com>
2019-02-24 15:24:58 +0800

    Update 2.runserver.py

--- 2.runserver.py
+++ 2.runserver.py
@@ -1,5 +1,8 @@
 '''
-创建数据库：
+这里简单学习了下什么是迁移数据库及尝试运行内置服务器 
+    
+    创建数据库：
+    
 我们将修改数据库称之为 迁移数据库
 使用迁移命令 migrate
 (11_env) PS D:\learning_log> python manage.py migrate
@@ -7,9 +10,10 @@
 在使用SQLite（一种使用单个文件的数据库）的新项目首次执行时，Django将新建一个数据库
 使用 ls 命令查看，可以发现文件夹多了个文件 db.sqlite3
 
-查看是否创建项目，runserver
+    
+    查看是否创建项目，runserver：
+    
 pyhon manage.py runserver 启动内置服务器
-
 这里我尝试了使用 PyCharm 的命令行 Terminal 貌似并没有配置，所以报错
 （PyCharm-属性-高级-使用管理员运行，成功解决）
 使用PowerShell（管理员），cd blog 至目录下再使用python manage.py runserver 成功启动
