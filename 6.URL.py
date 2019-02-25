ada6766489c2fa49405c3fe248fca97b1dcca4ea
qoften <qoften@qq.com>
2019-02-25 14:05:59 +0800

    学习创建网页三个阶段：定义URL，编写模版，编写视图。了解正则表达式regex

--- 6.URL.py
+++ 6.URL.py
@@ -17,7 +17,7 @@ URL模式描述了URL是如何设计，让Django知道如何将浏览器请求�
 之前runserver后，基础URL（http://localhost:8000/）返回默认Django网站，以提示我们正确建立项目
 现在要将基础URL映射到“学习笔记”主页
 
-    打开项目主文件夹（learning_log）的urls.py
+    打开项目主文件夹（learning_log）的urls.py：
 （书里显示与实际使用新版不同，后续代码已按新版重新编排）
 
 from django.conf.urls import include, url # 导入了管理URL的函数和模块
@@ -39,8 +39,8 @@ urlpatterns = [
     path('', 'app1.urls', namespace='app1'), # ''内没有，代表默认的首页，实参 namespace 可将app1的URL与其他分开，便于后续扩展（别忘了末尾的,）
 ]
 
-
     在app1文件夹下添加urls.py（新版2.1.7方法）：
+    
 （我是PyCharm打开项目，app1文件夹-鼠标右键-New-File-输入urls.py ）
 
 '''定义app1的URL模式''' #首行添加文档字符串，以区分是哪个app的urls.py
@@ -67,6 +67,18 @@ URL模式是一个对函数url（）的调用，函数接受三个实参
 >>>>第三个实参，将URL模式名称指定为'index'，以方便我们再其他地方引用它，需提供链接时，使用该名称而不用编写URL
 
 
+    编写模版index.html：
+    
+模版 定义了网页的结构，指定了网页是什么样子。模版让你能够访问视图提供的任何数据。
+app1中新建一个文件夹，命名为templates，该文件夹中再建立一个文件夹logs（建立Django可以明确解读的结构）
+在最里面的logs中，新建一个File 命名为index.html，在该文件中写入
+<p>Learning Log</p> # 标签<p></p>标识段落，<p>指出开头，</p>标出结尾
+
+<p>Learning Log helps you keep track of your learning, for any topic you're learning about.</p>
+
+# 这里定义了两个段落，第一个标题，第二个阐述用途
+
+
     编写视图views.py：
     
 app1中的 views.py 是由之前 python manage.py startapp 时自动生成
@@ -76,9 +88,10 @@ def index(reuest):
     '''学习笔记主页'''
     return render(request, 'app1/index.html') # 两个实参，原始请求对象和创建网页模版
 
-    编写index.html：
-    
-app1中新建一个文件夹，命名为templates，该文件夹中再建立一个文件夹logs
+
+再次请求基础URL（http://localhost:8000/admin/），转到刚才创建的网页
+Django接受请求的URL，发现该URL与模式''匹配，因此调用函数views.index(),使用index.html包含的模版渲染网页并展示
+
 
 
 
