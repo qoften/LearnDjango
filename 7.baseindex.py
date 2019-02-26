16df83227c9131674094a5476dd843bbf81e7470
qoften <qoften@qq.com>
2019-02-26 16:19:02 +0800

    Update 7.baseindex.py

--- 7.baseindex.py
+++ 7.baseindex.py
@@ -1,5 +1,5 @@
 '''
-
+请在本节起，将上节的logs文件夹替换为app1，代码中所有logs均替换为app1，避免html冲突！！！
 
   模板继承：
 
@@ -15,12 +15,12 @@
 在之前的 index.html 目录中，创建一个名为 base.html 的父模板，包含所有公用元素。
 当前公用部分只包含顶端标题，因此我们这么设置
 <p>
-    <a href="{% url 'logs:index' %}">Learning Log</a> # 创建段落，使用模板标签创建链接，段落包含项目名
+    <a href="{% url 'app1:index' %}">Learning Log</a> # 创建段落，使用模板标签创建链接，段落包含项目名
 </p>
 
 {% block content %}{% endblock content %} # 一对块标签，名为content，作为占位符，信息由子模块指定
 
-模版标签 用｛% %｝表示，{% url 'logs:index' %} 中，生成一个url与app1中views.py指向的urls.py定义URL模式匹配
+模版标签 用｛% %｝表示，{% url 'app1:index' %} 中，生成一个url与app1中views.py指向的urls.py定义URL模式匹配
 在这里 logs 是一个命名空间，index是该命名空间名称独特的URL模式
 
 在简单HTML页面中，链接使用 锚标签 定义：<a href="link_url">link text</a>
@@ -30,7 +30,7 @@
   子模板：
     
 重新编写 index.html，使其继承 base.html：
-｛% extends "logs/base.html" %｝ # 原标题替换从父模板继承的代码，让Django知道继承了哪个父模板
+｛% extends "app1/base.html" %｝ # 原标题替换从父模板继承的代码，让Django知道继承了哪个父模板
 
 {% block content %} # 定义content 不是从父模板继承的内容都包含在content块中
 
